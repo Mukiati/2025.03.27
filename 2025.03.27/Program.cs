1b13d174fa63f510c78825551f1839dccce07b0b
Bertók Attila <bertok.attila.21i@egressy.info>
2025-03-27 12:35:53 +0100

    Add project files.

---
+++ 2025.03.27/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025._03._27
+{
+    class Program
+    {
+        static serverconnection connection;
+        static void Main(string[] args)
+        {
+            
+            start();
+            Console.ReadKey();
+            connection = new serverconnection("http://localhost:3000");
+
+        }
+        static string name;
+        static int ara;
+        static float ertekeles;
+        static void start()
+        {
+            Console.WriteLine("Mit szeretnél?");
+            string input = Console.ReadLine();
+            if (input == "vásárlás")
+            {
+                Console.WriteLine(input);
+
+                Console.WriteLine("kolbász nevét add meg");
+                name = Console.ReadLine();
+                Console.WriteLine("kolbász árát add meg");
+                 ara = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("kolbász értékét add meg");
+                ertekeles = float.Parse(Console.ReadLine());
+                Console.WriteLine(name);
+                Console.WriteLine(ara);
+                Console.WriteLine(ertekeles);
+                create(name, ara, ertekeles);
+               
+
+            }
+            else if (input == "nézelődni")
+            {
+                Console.WriteLine(input);
+                kolbik();
+            }
+            else if (input == "törölni")
+            {
+                Console.WriteLine(input);
+            }
+
+
+        }
+         public static async void create(string inputname, int inputprice,float inputrating)
+        {
+            try
+            {
+                bool temp = await connection.Register( inputprice, inputname,inputrating);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+        static async void kolbik()
+        {
+            List<jasondata> all = await connection.Kolbaszok();
+            foreach (jasondata item in all)
+            {
+                Console.WriteLine(item.kolbaszNeve,item.kolbaszAra,item.kolbaszErtekelese);
+            }
+        }
+    }
+}
---
+++ 2025.03.27/serverconnection.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Windows;
+
+namespace _2025._03._27
+{
+   public  class serverconnection
+    {
+        HttpClient client = new HttpClient();
+        string serverUrl = "";
+        public serverconnection(string serverUrl)
+        {
+            this.serverUrl = serverUrl;
+        }
+        public async Task<List<string>> Kolbaszok()
+        {
+            List<string> all = new List<string>();
+            string url = serverUrl + "/kolbaszok";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                all = JsonConvert.DeserializeObject<List<jasondata>>(result).Select(item => item.kolbaszNeve).ToList();
+                result.ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return all;
+        }
+        public async Task<bool> Register(int kolbaszAra, string kolbaszNeve,float kolbaszErtekelese)
+        {
+            string url = serverUrl + "/createKolbasz";
+            try
+            {
+                var jsonInfo = new
+                {
+                    name = kolbaszNeve,
+                    ara = kolbaszAra,
+                    ertekeles = kolbaszErtekelese
+                };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                jasondata data = JsonConvert.DeserializeObject<jasondata>(result);
+               
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            return false;
+        }
+        public async Task<bool> deleteklbasz(int id)
+        {
+            string url = serverUrl + "/deleteKolbasz/:id";
+            try
+            {
+                var jsonInfo = new { id= id };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                jasondata data = JsonConvert.DeserializeObject<jasondata>(result);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return false;
+        }
+
+    }
+}
